44fed48ab709e1bf15e52eefc19595c954d85c92
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-23 15:58:47 +0100

    frame + grid layout

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,37 +1,43 @@
 package sk.stuba.fei.uim.oop;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class Main {
     public static void main(String[] args) {
-        SvetoveStrany strana = SvetoveStrany.JUH;
+        JFrame okno = new JFrame();
+        okno.setVisible(true);
+        okno.setSize(300,300);
+        okno.setResizable(false);
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setLayout(new GridLayout(3,4));
+        JLabel label = new JLabel("Hello world");
+        okno.add(label);
 
-        for (SvetoveStrany s : SvetoveStrany.values()) {
-            checkSever(s);
-        }
-
-        checkSever(SvetoveStrany.fromString("SEVER"));
-        checkSever(strana);
-        checkSever(SvetoveStrany.fromString("sever"));
-
-        switch (strana) {
-            case SEVER:
-                System.out.println("Je sever");
-                break;
-            default:
-                System.out.println("Nie je sever");
-                break;
+        for (int i = 0; i<10; i++){
+            okno.add(new JLabel("Hello " + i));
         }
 
     }
-        public static void checkSever(SvetoveStrany s){
-            if (s.isJeSever()) {
-                System.out.println("Je sever");
-            } else {
-                System.out.println("Nie je sever");
-            }
-        }
-    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
