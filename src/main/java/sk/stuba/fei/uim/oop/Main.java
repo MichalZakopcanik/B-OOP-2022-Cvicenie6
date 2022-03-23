f94537e818a1221ff6a7c6d3f318ed0360631895
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-23 16:05:06 +0100

    grid layout + panel v paneli + farby

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,5 +1,6 @@
 package sk.stuba.fei.uim.oop;
 
+import javax.security.auth.login.AppConfigurationEntry;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -11,14 +12,28 @@ public class Main {
         JFrame okno = new JFrame();
         okno.setVisible(true);
         okno.setSize(300,300);
-        okno.setResizable(false);
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         okno.setLayout(new GridLayout(3,4));
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,2));
+
+        for(int i=0; i<4; i++){
+            panel.add(new JLabel("Panel " + i));
+        }
+
+        panel.setBackground(new Color(190,120,150));
+        okno.getContentPane().setBackground(Color.YELLOW);
+
         JLabel label = new JLabel("Hello world");
         okno.add(label);
 
-        for (int i = 0; i<10; i++){
-            okno.add(new JLabel("Hello " + i));
+        for (int i = 0; i<10; i++) {
+            if (i == 8) {
+                okno.add(panel);
+            } else {
+                okno.add(new JLabel("Hello " + i));
+            }
         }
 
     }
